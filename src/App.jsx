f8b1a8ba36dda8bbd4b766c327f32ecfe843a56b
marc-antoine <marc-antoine.da.silva@efrei.net>
2023-03-03 11:36:42 +0100

    change setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import plantLogo from './assets/Logos/logo_plant_mascote.png'
 
@@ -9,17 +8,11 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
         <a href="#" target="_blank">
           <img src={plantLogo} className="plant_logo" alt="Plant logo" />
         </a>
       </div>
-      <h1>Vite + React + Plant</h1>
+      <h1>Suitaplante</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
